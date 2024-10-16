8b05e4fa66185f7fc6f18f6fc508c90eb6897d6d
Martini <marcino064@o2.pl>
2024-10-16 18:21:34 +0200

    Lab2 functions declared.

--- Labs/selekcja.py
+++ Labs/selekcja.py
@@ -1,1 +1,13 @@
 from rastrigin import *
+
+
+def selekcja_turniejowa(populacja, minimum=True):
+    pass
+
+
+def selekcja_rankingu(populacja, minimum=True):
+    pass
+
+
+def selekcja_ruletki(populacja, minimum=True):
+    pass
