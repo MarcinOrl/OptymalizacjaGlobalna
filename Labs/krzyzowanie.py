fcab89acf9f4a2939ba66893b940c2f483397aca
Martini <marcino064@o2.pl>
2024-11-03 14:31:30 +0100

    Implemented other crossovers.

--- Labs/krzyzowanie.py
+++ Labs/krzyzowanie.py
@@ -1,12 +1,111 @@
-from rastrigin import generuj_populacje, oblicz_bity
+from main import Populacja, Osobnik
 from config import rozmiar_populacji, dokladnosci, granice
-from osobnik import Osobnik
 import random
 
 
-def krzyzowanie(populacja, prawdopodobienstwo, liczba_punktow, liczba_bitow):
-    wylosowana_populacja = []
+def krzyzowanie_jednopunktowe(pary):
     nowa_populacja = []
+
+    for rodzic1, rodzic2 in pary:
+        print(f"Przed krzyzowaniem:\n {rodzic1.genotyp}, {rodzic2.genotyp}")
+        punkt = random.randint(0, len(rodzic1.genotyp) - 1)
+        nowy_osobnik1 = rodzic1.genotyp[:punkt] + rodzic2.genotyp[punkt:]
+        nowy_osobnik2 = rodzic2.genotyp[:punkt] + rodzic1.genotyp[punkt:]
+        nowa_populacja.append(Osobnik(nowy_osobnik1))
+        nowa_populacja.append(Osobnik(nowy_osobnik2))
+        print(
+            f"Po krzyzowaniu:\nLosowy punkt: {punkt}\n {nowy_osobnik1}, {nowy_osobnik2}"
+        )
+
+    return nowa_populacja
+
+
+def krzyzowanie_dwupunktowe(pary):
+    nowa_populacja = []
+
+    for rodzic1, rodzic2 in pary:
+        print(f"Przed krzyzowaniem:\n {rodzic1.genotyp}, {rodzic2.genotyp}")
+        punkt1 = random.randint(0, len(rodzic1.genotyp) - 1)
+        punkt2 = random.randint(0, len(rodzic1.genotyp) - 1)
+        if punkt1 > punkt2:
+            punkt1, punkt2 = punkt2, punkt1
+        nowy_osobnik1 = (
+            rodzic1.genotyp[:punkt1]
+            + rodzic2.genotyp[punkt1:punkt2]
+            + rodzic1.genotyp[punkt2:]
+        )
+        nowy_osobnik2 = (
+            rodzic2.genotyp[:punkt1]
+            + rodzic1.genotyp[punkt1:punkt2]
+            + rodzic2.genotyp[punkt2:]
+        )
+        nowa_populacja.append(Osobnik(nowy_osobnik1))
+        nowa_populacja.append(Osobnik(nowy_osobnik2))
+        print(
+            f"Po krzyzowaniu:\nLosowe punkty: {punkt1}, {punkt2}\n {nowy_osobnik1}, {nowy_osobnik2}"
+        )
+
+    return nowa_populacja
+
+
+def krzyzowanie_wielopunktowe(pary):
+    nowa_populacja = []
+
+    for rodzic1, rodzic2 in pary:
+        print(f"Przed krzyzowaniem:\n {rodzic1.genotyp}, {rodzic2.genotyp}")
+        liczba_punktow = random.randint(1, len(rodzic1.genotyp))
+        punkty = sorted(random.sample(range(len(rodzic1.genotyp)), liczba_punktow))
+        nowy_osobnik1 = []
+        nowy_osobnik2 = []
+
+        start = 0
+        for punkt in punkty:
+            if start % 2 == 0:
+                nowy_osobnik1 += rodzic1.genotyp[start:punkt]
+                nowy_osobnik2 += rodzic2.genotyp[start:punkt]
+            else:
+                nowy_osobnik1 += rodzic2.genotyp[start:punkt]
+                nowy_osobnik2 += rodzic1.genotyp[start:punkt]
+            start = punkt
+
+        nowy_osobnik1 += rodzic1.genotyp[start:]
+        nowy_osobnik2 += rodzic2.genotyp[start:]
+
+        nowa_populacja.append(Osobnik(nowy_osobnik1))
+        nowa_populacja.append(Osobnik(nowy_osobnik2))
+        print(
+            f"Po krzyzowaniu:\nLosowe punkty: {punkty}\n {nowy_osobnik1}, {nowy_osobnik2}"
+        )
+
+    return nowa_populacja
+
+
+def krzyzowanie_rownomierne(pary):
+    nowa_populacja = []
+
+    for rodzic1, rodzic2 in pary:
+        print(f"Przed krzyzowaniem:\n {rodzic1.genotyp},\n {rodzic2.genotyp}")
+        wzorzec = [random.randint(0, 1) for _ in range(len(rodzic1.genotyp))]
+        nowy_osobnik1 = [
+            rodzic1.genotyp[i] if wzorzec[i] == 0 else rodzic2.genotyp[i]
+            for i in range(len(rodzic1.genotyp))
+        ]
+        nowy_osobnik2 = [
+            rodzic2.genotyp[i] if wzorzec[i] == 0 else rodzic1.genotyp[i]
+            for i in range(len(rodzic1.genotyp))
+        ]
+        nowa_populacja.append(Osobnik(nowy_osobnik1))
+        nowa_populacja.append(Osobnik(nowy_osobnik2))
+        print(
+            f"Po krzyzowaniu:\nWzorzec: {wzorzec}\n {nowy_osobnik1},\n {nowy_osobnik2}"
+        )
+
+    return nowa_populacja
+
+
+def krzyzowanie(populacja, prawdopodobienstwo, typ):
+    wylosowana_populacja = []
+
     for osobnik in populacja:
         if random.random() < prawdopodobienstwo:
             wylosowana_populacja.append(osobnik)
@@ -15,27 +114,29 @@ def krzyzowanie(populacja, prawdopodobienstwo, liczba_punktow, liczba_bitow):
             wylosowana_populacja.pop(random.randint(0, len(wylosowana_populacja) - 1))
         elif len(wylosowana_populacja) == 1:
             wylosowana_populacja.append()
+
     random.shuffle(wylosowana_populacja)
     pary = [
         (wylosowana_populacja[i], wylosowana_populacja[i + 1])
         for i in range(0, len(wylosowana_populacja), 2)
     ]
-    for osobnik1, osobnik2 in pary:
-        print(f"Przed krzyzowaniem:\n {osobnik1.genotyp}, {osobnik2.genotyp}")
-        punkt = random.randint(0, len(osobnik1.genotyp) - 1)
-        nowy_osobnik1 = osobnik1.genotyp[:punkt] + osobnik2.genotyp[punkt:]
-        nowy_osobnik2 = osobnik2.genotyp[:punkt] + osobnik1.genotyp[punkt:]
-        nowa_populacja.append(Osobnik(nowy_osobnik1, liczba_bitow))
-        nowa_populacja.append(Osobnik(nowy_osobnik2, liczba_bitow))
-        print(f"Po krzyzowaniu:\n {nowy_osobnik1}, {nowy_osobnik2}")
-    return nowa_populacja
+
+    match typ:
+        case "jednopunktowe":
+            krzyzowanie_jednopunktowe(pary)
+        case "dwupunktowe":
+            krzyzowanie_dwupunktowe(pary)
+        case "wielopunktowe":
+            krzyzowanie_wielopunktowe(pary)
+        case "rownomierne":
+            krzyzowanie_rownomierne(pary)
+        case _:
+            raise ValueError(f"Nieznany typ krzyzowania: {typ}")
 
 
 if __name__ == "__main__":
-    liczba_bitow = [oblicz_bity(a, b, d) for (a, b), d in zip(granice, dokladnosci)]
+    # Tworzenie populacji
+    populacja = Populacja(rozmiar_populacji, granice, dokladnosci)
 
-    populacja = generuj_populacje(rozmiar_populacji, liczba_bitow)
-
-    populacja_osobnikow = [Osobnik(genotyp, liczba_bitow) for genotyp in populacja]
-
-    print(krzyzowanie(populacja_osobnikow, 0.5, 1, liczba_bitow))
+    # Wykonywanie krzyzowania
+    krzyzowanie(populacja.osobniki, 0.5, "jednopunktowe")
--- Labs/osobnik.py
+++
@@ -1,9 +0,0 @@
-from rastrigin import dekoduj_osobnika, rastrigin
-from config import granice
-
-
-class Osobnik:
-    def __init__(self, genotyp, liczba_bitow):
-        self.genotyp = genotyp
-        self.wartosci = dekoduj_osobnika(genotyp, granice, liczba_bitow)
-        self.wartosc_funkcji = rastrigin(self.wartosci)
