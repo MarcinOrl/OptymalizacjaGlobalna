457ea53caec0f41e5e7a72f66bafd066f5965782
Martini <marcino064@o2.pl>
2024-11-07 15:40:55 +0100

    Code cleanup.

--- Labs/krzyzowanie.py
+++ Labs/krzyzowanie.py
@@ -3,117 +3,13 @@ from config import rozmiar_populacji, dokladnosci, granice
 import random
 
 
-def krzyzowanie_jednopunktowe(pary):
-    nowa_populacja = []
-
-    for rodzic1, rodzic2 in pary:
-        print(f"Przed krzyzowaniem:\n {rodzic1.genotyp}, {rodzic2.genotyp}")
-        punkt = random.randint(0, len(rodzic1.genotyp) - 1)
-        nowy_osobnik1 = rodzic1.genotyp[:punkt] + rodzic2.genotyp[punkt:]
-        nowy_osobnik2 = rodzic2.genotyp[:punkt] + rodzic1.genotyp[punkt:]
-        nowa_populacja.append(Osobnik(nowy_osobnik1))
-        nowa_populacja.append(Osobnik(nowy_osobnik2))
-        print(
-            f"Po krzyzowaniu:\nLosowy punkt: {punkt}\n {nowy_osobnik1}, {nowy_osobnik2}"
-        )
-
-    return nowa_populacja
-
-
-def krzyzowanie_dwupunktowe(pary):
-    nowa_populacja = []
-
-    for rodzic1, rodzic2 in pary:
-        print(f"Przed krzyzowaniem:\n {rodzic1.genotyp}, {rodzic2.genotyp}")
-        punkt1 = random.randint(0, len(rodzic1.genotyp) - 1)
-        punkt2 = random.randint(0, len(rodzic1.genotyp) - 1)
-        if punkt1 > punkt2:
-            punkt1, punkt2 = punkt2, punkt1
-        nowy_osobnik1 = (
-            rodzic1.genotyp[:punkt1]
-            + rodzic2.genotyp[punkt1:punkt2]
-            + rodzic1.genotyp[punkt2:]
-        )
-        nowy_osobnik2 = (
-            rodzic2.genotyp[:punkt1]
-            + rodzic1.genotyp[punkt1:punkt2]
-            + rodzic2.genotyp[punkt2:]
-        )
-        nowa_populacja.append(Osobnik(nowy_osobnik1))
-        nowa_populacja.append(Osobnik(nowy_osobnik2))
-        print(
-            f"Po krzyzowaniu:\nLosowe punkty: {punkt1}, {punkt2}\n {nowy_osobnik1}, {nowy_osobnik2}"
-        )
-
-    return nowa_populacja
-
-
-def krzyzowanie_wielopunktowe(pary):
-    nowa_populacja = []
-
-    for rodzic1, rodzic2 in pary:
-        print(f"Przed krzyzowaniem:\n {rodzic1.genotyp}, {rodzic2.genotyp}")
-        liczba_punktow = random.randint(1, len(rodzic1.genotyp))
-        punkty = sorted(random.sample(range(len(rodzic1.genotyp)), liczba_punktow))
-        nowy_osobnik1 = []
-        nowy_osobnik2 = []
-
-        start = 0
-        for punkt in punkty:
-            if start % 2 == 0:
-                nowy_osobnik1 += rodzic1.genotyp[start:punkt]
-                nowy_osobnik2 += rodzic2.genotyp[start:punkt]
-            else:
-                nowy_osobnik1 += rodzic2.genotyp[start:punkt]
-                nowy_osobnik2 += rodzic1.genotyp[start:punkt]
-            start = punkt
-
-        nowy_osobnik1 += rodzic1.genotyp[start:]
-        nowy_osobnik2 += rodzic2.genotyp[start:]
-
-        nowa_populacja.append(Osobnik(nowy_osobnik1))
-        nowa_populacja.append(Osobnik(nowy_osobnik2))
-        print(
-            f"Po krzyzowaniu:\nLosowe punkty: {punkty}\n {nowy_osobnik1}, {nowy_osobnik2}"
-        )
-
-    return nowa_populacja
-
-
-def krzyzowanie_rownomierne(pary):
-    nowa_populacja = []
-
-    for rodzic1, rodzic2 in pary:
-        print(f"Przed krzyzowaniem:\n {rodzic1.genotyp},\n {rodzic2.genotyp}")
-        wzorzec = [random.randint(0, 1) for _ in range(len(rodzic1.genotyp))]
-        nowy_osobnik1 = [
-            rodzic1.genotyp[i] if wzorzec[i] == 0 else rodzic2.genotyp[i]
-            for i in range(len(rodzic1.genotyp))
-        ]
-        nowy_osobnik2 = [
-            rodzic2.genotyp[i] if wzorzec[i] == 0 else rodzic1.genotyp[i]
-            for i in range(len(rodzic1.genotyp))
-        ]
-        nowa_populacja.append(Osobnik(nowy_osobnik1))
-        nowa_populacja.append(Osobnik(nowy_osobnik2))
-        print(
-            f"Po krzyzowaniu:\nWzorzec: {wzorzec}\n {nowy_osobnik1},\n {nowy_osobnik2}"
-        )
-
-    return nowa_populacja
-
-
 def krzyzowanie(populacja, prawdopodobienstwo, typ):
-    wylosowana_populacja = []
+    wylosowana_populacja = [
+        osobnik for osobnik in populacja if random.random() < prawdopodobienstwo
+    ]
 
-    for osobnik in populacja:
-        if random.random() < prawdopodobienstwo:
-            wylosowana_populacja.append(osobnik)
-    if len(wylosowana_populacja) > 0:
-        if len(wylosowana_populacja) % 2 == 1 and len(wylosowana_populacja) > 1:
-            wylosowana_populacja.pop(random.randint(0, len(wylosowana_populacja) - 1))
-        elif len(wylosowana_populacja) == 1:
-            wylosowana_populacja.append()
+    if len(wylosowana_populacja) % 2 == 1:
+        wylosowana_populacja.pop(random.randint(0, len(wylosowana_populacja) - 1))
 
     random.shuffle(wylosowana_populacja)
     pary = [
@@ -121,22 +17,60 @@ def krzyzowanie(populacja, prawdopodobienstwo, typ):
         for i in range(0, len(wylosowana_populacja), 2)
     ]
 
-    match typ:
-        case "jednopunktowe":
-            krzyzowanie_jednopunktowe(pary)
-        case "dwupunktowe":
-            krzyzowanie_dwupunktowe(pary)
-        case "wielopunktowe":
-            krzyzowanie_wielopunktowe(pary)
-        case "rownomierne":
-            krzyzowanie_rownomierne(pary)
-        case _:
-            raise ValueError(f"Nieznany typ krzyzowania: {typ}")
+    for rodzic1, rodzic2 in pary:
+        match typ:
+            case "jednopunktowe":
+                punkt = random.randint(0, len(rodzic1.genotyp) - 1)
+                # print(f"Punkt: {punkt}")
+                # print(f"Przed krzyżowaniem: {rodzic1.genotyp}, {rodzic2.genotyp}")
+                rodzic1.genotyp[punkt:], rodzic2.genotyp[punkt:] = (
+                    rodzic2.genotyp[punkt:],
+                    rodzic1.genotyp[punkt:],
+                )
+                # print(f"Po krzyżowaniu: {rodzic1.genotyp}, {rodzic2.genotyp}")
+            case "dwupunktowe":
+                punkt1, punkt2 = sorted(random.sample(range(len(rodzic1.genotyp)), 2))
+                rodzic1.genotyp[punkt1:punkt2], rodzic2.genotyp[punkt1:punkt2] = (
+                    rodzic2.genotyp[punkt1:punkt2],
+                    rodzic1.genotyp[punkt1:punkt2],
+                )
+            case "wielopunktowe":
+                liczba_punktow = random.randint(1, len(rodzic1.genotyp))
+                punkty = sorted(
+                    random.sample(range(len(rodzic1.genotyp)), liczba_punktow)
+                )
+                start = 0
+                for i, punkt in enumerate(punkty):
+                    if i % 2 == 0:
+                        rodzic1.genotyp[start:punkt], rodzic2.genotyp[start:punkt] = (
+                            rodzic2.genotyp[start:punkt],
+                            rodzic1.genotyp[start:punkt],
+                        )
+                    start = punkt
+            case "rownomierne":
+                wzorzec = [random.randint(0, 1) for _ in range(len(rodzic1.genotyp))]
+                rodzic1.genotyp = [
+                    rodzic1.genotyp[i] if wzorzec[i] == 0 else rodzic2.genotyp[i]
+                    for i in range(len(rodzic1.genotyp))
+                ]
+                rodzic2.genotyp = [
+                    rodzic2.genotyp[i] if wzorzec[i] == 0 else rodzic1.genotyp[i]
+                    for i in range(len(rodzic2.genotyp))
+                ]
+            case _:
+                raise ValueError(f"Nieznany typ krzyzowania: {typ}")
+
+    return populacja
 
 
 if __name__ == "__main__":
     # Tworzenie populacji
     populacja = Populacja(rozmiar_populacji, granice, dokladnosci)
 
+    for i, osobnik in enumerate(populacja.osobniki):
+        print(f"Osobnik {i + 1}: {osobnik.genotyp}")
     # Wykonywanie krzyzowania
     krzyzowanie(populacja.osobniki, 0.5, "jednopunktowe")
+
+    for i, osobnik in enumerate(populacja.osobniki):
+        print(f"Osobnik {i + 1}: {osobnik.genotyp}")
--- Labs/mutacja_inwersja.py
+++ Labs/mutacja_inwersja.py
@@ -6,11 +6,11 @@ import random
 # Funkcja mutacji odwracająca bity w zależności od prawdopodobieństwa
 def mutacja(populacja, prawdopodobienstwo):
     for osobnik in populacja:
-        print(f"Przed mutacją: {osobnik.genotyp}")
+        # print(f"Przed mutacją: {osobnik.genotyp}")
         for i in range(len(osobnik.genotyp)):
             if random.random() < prawdopodobienstwo:
                 osobnik.genotyp[i] = 1 - osobnik.genotyp[i]  # Odwracanie bitu
-        print(f"Po mutacji:    {osobnik.genotyp}\n------------------------")
+        # print(f"Po mutacji:    {osobnik.genotyp}\n------------------------")
 
     return populacja
 
@@ -19,11 +19,11 @@ def mutacja(populacja, prawdopodobienstwo):
 def inwersja(populacja, prawdopodobienstwo):
     for osobnik in populacja:
         if random.random() < prawdopodobienstwo:
-            print(f"Przed inwersją: {osobnik.genotyp}")
+            # print(f"Przed inwersją: {osobnik.genotyp}")
             start = random.randint(0, len(osobnik.genotyp) - 2)
             end = random.randint(start + 1, len(osobnik.genotyp) - 1)
             osobnik.genotyp[start : end + 1] = osobnik.genotyp[start : end + 1][::-1]
-            print(f"Po inwersji:    {osobnik.genotyp}\n------------------------")
+            # print(f"Po inwersji:    {osobnik.genotyp}\n------------------------")
 
     return populacja
 
@@ -32,6 +32,15 @@ if __name__ == "__main__":
     # Tworzenie populacji osobników
     populacja = Populacja(rozmiar_populacji, granice, dokladnosci)
 
+    for i, osobnik in enumerate(populacja.osobniki):
+        print(f"Wartość funkcji {i}: {osobnik.wartosc_funkcji}")
     # Wykonywanie mutacji i inwersji
-    mutowana_populacja = mutacja(populacja.osobniki, 0.1)
-    inwertowana_populacja = inwersja(populacja.osobniki, 0.1)
+    populacja.osobniki = mutacja(populacja.osobniki, 0.1)
+
+    for osobnik in populacja.osobniki:
+        populacja.dekoduj_osobnika(osobnik)
+
+    for i, osobnik in enumerate(populacja.osobniki):
+        print(f"Wartość funkcji {i}: {osobnik.wartosc_funkcji}")
+
+    # inwertowana_populacja = inwersja(populacja.osobniki, 0.1)
